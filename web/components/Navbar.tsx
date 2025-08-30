417a6306da1c8f4f52363ebd7b1424ea8d29b235
Edward Clark <edwardclark1104@gmail.com>
2025-08-30 14:50:59 +0100

    Add texture

--- web/components/Navbar.tsx
+++ web/components/Navbar.tsx
@@ -16,7 +16,7 @@ export function Navbar() {
   };
 
   return (
-    <nav className="bg-white border-b border-gray-100">
+    <nav className="bg-white border-b border-gray-100 sticky top-0 z-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex-shrink-0 flex items-center">
