bf0450a3213a96b2f0438a57971c867855b680ce
Jack Jarvis <jackjarvis1123@gmail.com>
2025-08-31 17:41:12 +0100

    Cover image migrations

---
+++ api/api/Migrations/20250831164052_AddCoverImageUrl.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCoverImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CoverImageUrl",
+                table: "Posts",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CoverImageUrl",
+                table: "Posts");
+        }
+    }
+}
