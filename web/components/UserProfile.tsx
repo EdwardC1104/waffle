6293ba6322b307ee65ea893674264114084d9c8f
Edward Clark <edwardclark1104@gmail.com>
2025-08-31 14:58:15 +0100

    Live follow count

--- web/components/UserProfile.tsx
+++ web/components/UserProfile.tsx
@@ -210,7 +210,7 @@ export default function UserProfile({
                 <div
                   className={`text-center text-stone-900 ${config.statsNumber} font-serif`}
                 >
-                  {formatNumber(0)}
+                  {formatNumber(user.followerCount)}
                 </div>
                 <div
                   className={`text-center text-stone-900 ${config.statsLabel}`}
@@ -226,7 +226,7 @@ export default function UserProfile({
                 <div
                   className={`text-center text-stone-900 ${config.statsNumber} font-serif`}
                 >
-                  {0}
+                  {user.followingCount}
                 </div>
                 <div
                   className={`text-center text-stone-900 ${config.statsLabel}`}
