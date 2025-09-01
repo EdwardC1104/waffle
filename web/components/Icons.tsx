ec1d509cf645238ce80e2e5be648acf85f0b7bd3
Jack Cains <jackcains101@gmail.com>
2025-09-01 13:13:42 +0100

    added saved to drop down and fixed inconsitencies

--- web/components/Icons.tsx
+++ web/components/Icons.tsx
@@ -177,30 +177,29 @@ export const ImageIcon = ({ size = 16, className = "" }: IconProps) => (
   </svg>
 );
 
-export const SavedPostsIcon = ({ size = 24, className = "" }: IconProps) => (
+export const SavedPostsIcon = ({ size = 20, className = "" }: IconProps) => (
   <svg
     width={size}
     height={size}
-    viewBox="0 0 24 24"
+    viewBox="0 0 20 20"
     fill="none"
-    xmlns="http://www.w3.org/2000/svg"
     className={className}
   >
     <path
-      d="M17 3H7C5.89543 3 5 3.89543 5 5V21L12 17L19 21V5C19 3.89543 18.1046 3 17 3Z"
+      d="M15 2.5H5C4.16667 2.5 3.5 3.16667 3.5 4V17.5L10 13.75L16.5 17.5V4C16.5 3.16667 15.8333 2.5 15 2.5Z"
       stroke="currentColor"
       strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
-      d="M9 9H15"
+      d="M7.5 7.5H12.5"
       stroke="currentColor"
       strokeWidth="1.5"
       strokeLinecap="round"
     />
     <path
-      d="M9 12H15"
+      d="M7.5 10H12.5"
       stroke="currentColor"
       strokeWidth="1.5"
       strokeLinecap="round"
