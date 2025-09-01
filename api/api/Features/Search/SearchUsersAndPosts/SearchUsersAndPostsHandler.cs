2fa8f97552ae167d34074ce8f70d09a9ff920330
jack-jarvis12 <157592857+jack-jarvis12@users.noreply.github.com>
2025-09-01 18:33:47 +0100

    Improve searching (#2)

--- api/api/Features/Search/SearchUsersAndPosts/SearchUsersAndPostsHandler.cs
+++ api/api/Features/Search/SearchUsersAndPosts/SearchUsersAndPostsHandler.cs
@@ -20,28 +20,39 @@ public class SearchUsersAndPostsHandler
     
     public async Task<SearchUsersAndPostsResponse> Handle(SearchUsersAndPostsQuery query)
     {
-        // Fetch posts including user
-        var postsEntities = await _dbContext.Posts
-            .ToListAsync(); // fetch first
+        var posts = await _dbContext.Posts.Where(p => EF.Functions.ToTsVector("english", p.Title + " " + p.Content)
+                .Matches(EF.Functions.PlainToTsQuery("english", query.Query)))            .Include(p => p.User)
+            .ToListAsync();
 
-        // Map to DTOs asynchronously
-        var posts = new List<PostDto>();
-        foreach (var post in postsEntities)
+        var postDtos = new List<PostDto>();
+        foreach (var post in posts)
         {
-            posts.Add(await post.ToDtoAsync(_dbContext));
+            var postDto = await post.ToDtoAsync(_dbContext);
+            postDtos.Add(postDto);
+        }
+        
+        var users = await _dbContext.Users.Where(u => EF.Functions.ToTsVector("english", u.Name + " " + u.UserName)
+            .Matches(EF.Functions.PlainToTsQuery("english", query.Query)))
+            .ToListAsync();
+        
+        var userDtos = new List<UserDto>();
+        foreach (var user in users)
+        {
+            var userDto = await user.ToDtoAsync(_dbContext);
+            userDtos.Add(userDto);
         }
 
         return new SearchUsersAndPostsResponse
         {
-            Users = [],
-            Posts = posts
+            Users = userDtos,
+            Posts = postDtos
         };
     }
     
     public async Task<SearchUsersAndPostsResponse> Handle(string userId, SearchUsersAndPostsQuery query)
     {
         var posts = await _dbContext.Posts.Where(p => EF.Functions.ToTsVector("english", p.Title + " " + p.Content)
-                .Matches(query.Query))
+                .Matches(EF.Functions.PlainToTsQuery("english", query.Query)))
             .Include(p => p.User)
             .ToListAsync();
 
@@ -53,7 +64,7 @@ public class SearchUsersAndPostsHandler
         }
         
         var users = await _dbContext.Users.Where(u => EF.Functions.ToTsVector("english", u.Name + " " + u.UserName)
-                .Matches(query.Query))
+            .Matches(EF.Functions.PlainToTsQuery("english", query.Query)))
             .ToListAsync();
         
         var userDtos = new List<UserDto>();
