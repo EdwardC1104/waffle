a3a3dcac200d08ce970d6872851026aee999cc13
jack-jarvis12 <157592857+jack-jarvis12@users.noreply.github.com>
2025-09-08 13:24:36 +0100

    🗑️ Change follow delete mode to Cascade (#46)

---
+++ api/api/Migrations/20250908100018_FollowDeleteCascade.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FollowDeleteCascade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Follows_AspNetUsers_FolloweeId",
+                table: "Follows");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Follows_AspNetUsers_FollowerId",
+                table: "Follows");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Follows_AspNetUsers_FolloweeId",
+                table: "Follows",
+                column: "FolloweeId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Follows_AspNetUsers_FollowerId",
+                table: "Follows",
+                column: "FollowerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Follows_AspNetUsers_FolloweeId",
+                table: "Follows");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Follows_AspNetUsers_FollowerId",
+                table: "Follows");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Follows_AspNetUsers_FolloweeId",
+                table: "Follows",
+                column: "FolloweeId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Follows_AspNetUsers_FollowerId",
+                table: "Follows",
+                column: "FollowerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
